3566d05d56a18c5337641c3599adf4af659f683b
Pullela Dheeraj Reddy <dheerajreddy258@gmail.com>
2024-06-07 12:08:10 +0530

    front-end

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,26 @@ const cors = require('cors');
 const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
+const allowedOrigins = ['http://localhost:3000','https://kanteen-ase.netlify.app']
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if(allowedOrigins.includes(origin)){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+app.use(express.json())
+
+// Use the appropriate module for the protocol
+const keepAlive = () => {
+    const url = new URL(process.env.SERVER_URL);
+    const protocol = url.protocol === 'https:' ? https : http;
+    protocol.get(process.env.SERVER_URL);
+};
+
+setInterval(keepAlive, 300000);
 const http = require('http');
 const server = http.createServer(app);
 
